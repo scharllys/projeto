750eddaf4790a14e71e6bd8a8381d385a7530f4c
scharllys <willianscharllys@gmail.com>
2025-03-28 21:49:18 -0300

    Conclução de conteúdo do projeto

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const infosfn = async(nome) =>{
     try{
     const resposta = await fetch(`https://api.github.com/users/${nome}`)
     const data = await resposta.json()
-    console.log(data)
+    let ctx = document.createElement('div') 
+    ctx = `
+    <div class="ctx">
+    <div>
+        <img src="">
+        <h2>${data.nome}</h2>
+        <p>${data.login}</p>
+    <div>
+    <div>   
+     <p> Bio: ${data.bio}</p>
+     <p>Seguidores:${data.followers} Seguindo:${data.following}</p>
+     <P>Repositórios públicos: ${data.public_repos}</p> 
+    <a>href="${data.html_url}" target="_blank">Acesse o perfil</a>
+    </div>
+    </div>
+     `
+    document.querySelector('.infos').innerHTML = ctx
+    console.log(data.nome)
     }catch(err){
      console.log(err)
     }
@@ -13,15 +30,14 @@ const infosfn = async(nome) =>{
 const reposfn = async(nome) =>{
     try{
         const resposta = await fetch(`https://api.github.com/users/${nome}/repos`)
-        const data = await resposta.json()
+        console.log(err)
+        const data=await resposta.json()
         console.log(data)
     }catch(err){
-        console.log(err)
-    }
-}
 
+     }
+}
 button.addEventListener('click',()=>{
     document.querySelector('.infos').innerHTML = input
-    reposfn(input.value)
     infosfn(input.value)
 })
